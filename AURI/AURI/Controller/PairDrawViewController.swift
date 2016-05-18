7ca203247aa7c97de1bdf04fb03dd927d480c53b
KikurageChan <cihi14057@g.nihon-u.ac.jp>
2016-05-18 22:18:56 +0900

    first commit

---
+++ AURI/AURI/Controller/PairDrawViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PairDrawViewController.swift
+//  AURI
+//
+//  Created by 木耳ちゃん on 2016/05/18.
+//  Copyright © 2016年 NeTGroup. All rights reserved.
+//
+
+import UIKit
+
+class PairDrawViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }   
+}
